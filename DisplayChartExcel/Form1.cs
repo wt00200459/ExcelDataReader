1bcce4dac800eb09e1e31db86a6ad1badb13b499
wt00200459 <boss.2587@hotmail.co.th>
2018-10-03 10:56:27 +0700

    add-display-to-project

--- DisplayChartExcel/Form1.cs
+++ DisplayChartExcel/Form1.cs
@@ -16,5 +16,20 @@ namespace DisplayChartExcel
         {
             InitializeComponent();
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
